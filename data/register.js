14a0b7ee5ea16cb4dc54f3194d0e819293dabf80
yhnuj111 <xuz10@miamioh.edu>
2019-04-23 23:55:29 -0400

    Insert to mongodb

--- data/register.js
+++ data/register.js
@@ -1,6 +1,31 @@
-const mongoCollections = require("./collection.js");
+const mongoCollections = require("./collections.js");
 const users = mongoCollections.Users;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 async function create(fname, lname, age, gender, city, state, email, phoneNumber, passWord) {
-    
+    const hashedPW = await bcrypt.hash(passWord, saltRounds);
+    const usersCollection = await users();
+    let userProfile = {
+        hashedPassword: hashedPW,
+        profile: {
+        firstName: fname,
+        lastName: lname,
+        age: age,
+        gender: gender,
+        City: city,
+        state: state,
+        Email: email,
+        phoneNumber: phoneNumber,
+        }, 
+        bookmark: []
+    };
+    console.log(userProfile);
+    const insertInfo = await usersCollection.insertOne(userProfile);
+    if (insertInfo.insertedCount === 0) throw "Created failed";
+    return userProfile;
 }
+
+module.exports = {
+    create
+};
--- routes/Register.js
+++ routes/Register.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const data = require("../data");
+const registerData = data.register;
+const path = require("path");
 
 // var popupS = require('popups');
 
 
 router.post("/", async (req, res) => {
     let userData = req.body;
-    console.log(userData.fname);
+    try {
+        const fname = userData.fname;
+        const lname = userData.lname;
+        const age = userData.age;
+        const gender = userData.gender;
+        const city = userData.city;
+        const state = userData.state;
+        const email = userData.email;
+        const phoneNumber = userData.phontNumber;
+        const password = userData.pw1;
+        const createAccount = await registerData.create(fname, lname, age, gender, city, state, email, phoneNumber, password);
+        res.sendFile(path.resolve("static/login.html"));
+    } catch(e) {
+        console.log(e);
+        res.status(500).send();
+    }
         
 });
 
