ba33cdf0a617330632cd9720146ee0cfc6e15c2f
Raymond Yin <raymondxiwei@gmail.com>
2019-05-14 17:55:05 -0400

    updated URL limitation and seed.js file

--- data/seed.js
+++ data/seed.js
@@ -17,12 +17,12 @@ const main = async () => {
     // userList.push(await register.create("Thor", "Odinson", "1500", "Male", "New Asgadian", "NY", "US", "thor-odinson@avengers.com", "201-111-1113", "iamodinson123"));
 
     // let bookmarkList = [];
-    await bookmark.addBookmark("search", "Google's search engine", "www.google.com", userTony._id.toString());
-    await bookmark.addBookmark("search", "Baidu's search engine, it searches Chinese characters well", "www.baidu.com", userTony._id.toString());
-    await bookmark.addBookmark("search", "Microsoft's search engine, it's okay but its marketshare is so sad", "www.bing.com", userTony._id.toString());
-    await bookmark.addBookmark("drama", "Game of Throne", "www.gameofthrone.com", userTony._id.toString());
-    await bookmark.addBookmark("drama", "Silicon Valley", "www.thesiliconvalley.com", userTony._id.toString());
-    await bookmark.addBookmark("drama", "Westworld", "www.westworld.com", userTony._id.toString());
+    await bookmark.addBookmark("search", "Google's search engine", "https://www.google.com", userTony._id.toString());
+    await bookmark.addBookmark("search", "Baidu's search engine, it searches Chinese characters well", "https://www.baidu.com", userTony._id.toString());
+    await bookmark.addBookmark("search", "Microsoft's search engine, it's okay but its marketshare is so sad", "https://www.bing.com", userTony._id.toString());
+    await bookmark.addBookmark("drama", "Game of Throne", "https://www.gameofthrone.com", userTony._id.toString());
+    await bookmark.addBookmark("drama", "Silicon Valley", "https://www.thesiliconvalley.com", userTony._id.toString());
+    await bookmark.addBookmark("drama", "Westworld", "https://www.westworld.com", userTony._id.toString());
     await bookmark.addBookmark("movie", "Avenger: Endgame", "https://www.wikiwand.com/en/Avengers:_Endgame", userTony._id.toString());
     // for (i in userList) {
     //     let userId = userList[i]._id;
