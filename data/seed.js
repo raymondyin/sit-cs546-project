bf38c3029cfe932e67665934dfe48542ec6f0e41
yhnuj111 <xuz10@miamioh.edu>
2019-05-14 14:53:06 -0400

    Revert "Update seed.js"
    This reverts commit 12d16de93427d6afa81e57060ecf2aa5f0bf30bf.

--- data/seed.js
+++ data/seed.js
@@ -10,14 +10,14 @@ const main = async () => {
     await db.dropDatabase();
 
     let userList = [];
-    userList.push(await register.create("Tony", "Stark", "45", "Male","New York", "NY", "US",  "tony-stark@avengers.com", "201-111-1111", "123456"));
-    userList.push(await register.create("Steve", "Rogers", "78", "Male", "New York", "NY", "US", "steve-rogers@avengers.com", "201-111-1112", "123456"));
-    userList.push(await register.create("Thor", "Odinson", "10", "Male", "New York", "NA", "US", "thor-odinson@avengers.com", "201-111-1113", "123456"));
+    userList.push(await register.create("Tony", "Stark", "45", "Male", "New York", "NY", "US", "tony-stark@avengers.com", "201-111-1111", "123"));
+    userList.push(await register.create("Steve", "Rogers", "78", "Male", "New York", "NY", "US", "steve-rogers@avengers.com", "201-111-1112", "123"));
+    userList.push(await register.create("Thor", "Odinson", "1500", "Male", "New Asgadian", "NY", "US", "thor-odinson@avengers.com", "201-111-1113", "123"));
 
     let bookmarkList = [];
-    for (let i in userList) {
+    for (i in userList) {
         let userId = userList[i]._id;
-        bookmarkList.push(await bookmark.addBookmark("search", "search engine", "www.google.com", userId.toString(), "No"));
+        bookmarkList.push(await bookmark.addBookmark("search", "search engine", "www.google.com", userId));
     }
     console.log("Done seeding database");
     await db.serverConfig.close();
